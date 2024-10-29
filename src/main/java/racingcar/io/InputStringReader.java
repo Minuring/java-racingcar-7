f6c51ba26b1f1a15ad3e87e98c26b4ecc3224c9e
Minuring <mw9505@gmail.com>
2024-10-29 20:34:03 +0900

    refactor(input): 입력 라벨 상수화

--- src/main/java/racingcar/io/InputStringReader.java
+++ src/main/java/racingcar/io/InputStringReader.java
@@ -4,13 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputStringReader {
 
+    private static final String CAR_NAMES_LABEL = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String COUNT_LABEL = "시도할 횟수는 몇 회인가요?";
+
     public String readCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(CAR_NAMES_LABEL);
         return Console.readLine();
     }
 
     public String readCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(COUNT_LABEL);
         return Console.readLine();
     }
 }
