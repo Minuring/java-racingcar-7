ea2d691f3438521da12a45363d79f67e57519e1a
Minuring <mw9505@gmail.com>
2024-10-29 21:08:03 +0900

    refactor(validation): 메세지 상수화

--- src/main/java/racingcar/application/validation/CarNamesValidator.java
+++ src/main/java/racingcar/application/validation/CarNamesValidator.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class CarNamesValidator implements Validator {
 
+    private static final String NOT_DUPLICATED = "자동차 이름은 중복될 수 없습니다.";
+    private static final String MUST_MORE_THAN_TWO = "자동차는 최소 두 대 이상 있어야합니다.";
+    private static final String NOT_CONTAINS_BLANK = "자동차 이름에는 공백이 있을 수 없습니다.";
+    private static final String EXCEED_MAX_LENGTH = "자동차 이름은 5자 이하여야 합니다.";
+
     @Override
     public void validate(String rawString) {
         List<String> nameList = List.of(rawString.split(","));
@@ -16,22 +21,22 @@ public class CarNamesValidator implements Validator {
         long distinctSize = nameList.stream().distinct().count();
 
         if (size != distinctSize) {
-            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+            throw new IllegalArgumentException(NOT_DUPLICATED);
         }
 
         if (size <= 1) {
-            throw new IllegalArgumentException("자동차는 최소 두 대 이상 있어야합니다.");
+            throw new IllegalArgumentException(MUST_MORE_THAN_TWO);
         }
     }
 
     private void validateEach(List<String> nameList) {
         nameList.forEach(name -> {
             if (name == null || name.isEmpty() || name.matches(".*\\s+.*")) {
-                throw new IllegalArgumentException("자동차 이름에는 공백이 있을 수 없습니다.");
+                throw new IllegalArgumentException(NOT_CONTAINS_BLANK);
             }
 
             if (name.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+                throw new IllegalArgumentException(EXCEED_MAX_LENGTH);
             }
         });
     }
--- src/main/java/racingcar/application/validation/CountValidator.java
+++ src/main/java/racingcar/application/validation/CountValidator.java
@@ -2,21 +2,25 @@ package racingcar.application.validation;
 
 public class CountValidator implements Validator {
 
+    private static final String EMPTY_CONTENTS = "횟수를 입력해주세요.";
+    private static final String TYPE_MUST_INTEGER = "정수만 입력 가능합니다.";
+    private static final String RANGE_OUT = "1이상 50이하의 정수만 입력 가능합니다.";
+
     @Override
     public void validate(String string) {
         if (string == null || string.isEmpty() || string.isBlank()) {
-            throw new IllegalArgumentException("횟수를 입력해주세요.");
+            throw new IllegalArgumentException(EMPTY_CONTENTS);
         }
 
         string = string.trim();
 
         if (!string.matches("\\d*")) {
-            throw new IllegalArgumentException("정수만 입력 가능합니다.");
+            throw new IllegalArgumentException(TYPE_MUST_INTEGER);
         }
 
         int value = Integer.parseInt(string);
         if (value <= 0 || value >= 51) {
-            throw new IllegalArgumentException("1이상 50이하의 정수만 입력 가능합니다.");
+            throw new IllegalArgumentException(RANGE_OUT);
         }
     }
 }
