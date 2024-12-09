b7ece411ff4eeae3f5e8f64d10c8846d5de3cc63
Minuring <mw9505@gmail.com>
2024-12-09 19:27:20 +0900

    feat: application
    main

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.strategy.RandomForwardCondition;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Car> cars = InputView.readCars();
+        int repetitions = InputView.readRepetitions();
+
+        RacingGame game = new RacingGame(cars);
+        game.proceed(repetitions, new RandomForwardCondition());
+        List<Car> winners = game.getWinners();
+
+        OutputView.printWinners(winners);
     }
 }
